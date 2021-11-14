973d83739deb2821c0ff8b383a60bb736c234e1f
SashaKlash <sashaderuyver@gmail.com>
2021-11-14 16:56:17 +0100

    modification menu

--- portefolio-construction/import/menu.php
+++ portefolio-construction/import/menu.php
@@ -1,21 +1,19 @@
 <nav id="menu">
-     <ul>
-        <li><a href="./?pg=home" class="nav" id="">Accueil</a></li>
-        <li><a href="./?pg=photo" class="nav" id="">Galerie</a></li>
-        <li><a href="./?pg=lien" class="nav" id="">Liens</a></li>
-        <li><a href="./?pg=tuto" class="nav" id="">Tutoriels</a></li>
-        <li><a href="./?pg=contact" class="nav" id="">Contact</a></li>
-        <li><a href="./?pg=cv" class="nav" id="">CV</a></li>
-        <li><a href="./?pg=admin" class="nav" id="">Admin</a></li>
-    </ul>
+        <a href="./?pg=home" class="nav" id="">Accueil</a>
+        <a href="./?pg=photo" class="nav" id="">Galerie</a>
+        <a href="./?pg=lien" class="nav" id="">Liens</a>
+        <a href="./?pg=tuto" class="nav" id="">Tutoriels</a>
+        <a href="./?pg=contact" class="nav" id="">Contact</a>
+        <a href="./?pg=cv" class="nav" id="">CV</a>
+        <a href="./?pg=admin" class="nav" id="">Admin</a>
  </nav>
 
  <nav id="menu2">
-    <span class="nav"><a href="./?pg=home" class="" id="">Accueil</a></span>
-    <span class="nav"><a href="./?pg=photo" class="" id="">Galerie</a></span>
-    <span class="nav"><a href="./?pg=lien" class="" id="">Liens</a></span>
-    <span class="nav"><a href="./?pg=tuto" class="" id="">Tutoriels</a></span>
-    <span class="nav"><a href="./?pg=cv" class="" id="">CV</a></span>
-    <span class="nav"><a href="./?pg=contact" class="" id="">Contact</a></span>
-    <span class="nav"><a href="./?pg=admin" class="" id="">Admin</a></span>
+   <span class="nav"><a href="./?pg=home" class="" id="">Accueil</a></span>
+   <span class="nav"><a href="./?pg=photo" class="" id="">Galerie</a></span>
+   <span class="nav"><a href="./?pg=lien" class="" id="">Liens</a></span>
+   <span class="nav"><a href="./?pg=tuto" class="" id="">Tutoriels</a></span>
+   <span class="nav"><a href="./?pg=cv" class="" id="">CV</a></span>
+   <span class="nav"><a href="./?pg=contact" class="" id="">Contact</a></span>
+   <span class="nav"><a href="./?pg=admin" class="" id="">Admin</a></span>
  </nav>
