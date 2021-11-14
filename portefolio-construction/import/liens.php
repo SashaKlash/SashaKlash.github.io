7b07391c0f8b54b7f6079ced96b51d12878d5acf
SashaKlash <sashaderuyver@gmail.com>
2021-11-14 16:57:45 +0100

    modification page liens.php

--- portefolio-construction/import/liens.php
+++ portefolio-construction/import/liens.php
@@ -2,7 +2,7 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,8 +23,12 @@
             <li><a href="https://fr.freepik.com/vecteurs-libre/tasse-cafe-expresso-grains-cafe_10578326.htm#page=1&position=8&from_view=search">FreePik</a><p>Ici, tu peux developper...</p></li>
             <li><a href="https://coolors.co">Coolors</a><p>Ici, tu peux choisir de magnifiques couleurs chatoyantes !</p></li>
         </ul>
-    </sec
-    <section class="HTML"></section>
+    </section>
+    <section class="HTML">
+        <h3>Tips HTML</h3>
+            <p>Voici une liste des différents événements possible : <a href="https://www.w3schools.com/tags/ref_eventattributes.asp">W3C</a></p>
+            <p>Liste des différents attributs et leur(s) tag(s) associés <a href="https://www.w3schools.com/tags/ref_attributes.asp">Ici</p>
+    </section>
     <section class="JS"></section>
     <section class="image">
         <ul>
