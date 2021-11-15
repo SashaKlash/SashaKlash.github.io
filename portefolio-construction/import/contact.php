f61c42cdb0abc866559c005d09d1f26669d6361d
Antame <Anne.Martine.META.MUSASA@cf2000.onmicrosoft.com>
2021-11-15 16:30:15 +0100

    commit de fin de journée

--- portefolio-construction/import/contact.php
+++ portefolio-construction/import/contact.php
@@ -13,8 +13,7 @@
 <body>
     
     <?php
-    // on peut importer le menu en PHP sans passer par index.php
-    include "header.php";
+        include "header.php";
     ?>
     <h1>contact</h1>
     <?php
