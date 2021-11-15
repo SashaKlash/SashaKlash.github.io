1458f41c9916a080a481adef62ec7693789ba135
SashaKlash <sashaderuyver@gmail.com>
2021-11-15 07:44:33 +0100

    modif style.css + menu.php

--- portefolio-construction/import/menu.php
+++ portefolio-construction/import/menu.php
@@ -1,12 +1,4 @@
-<nav id="menu">
-        <a href="./?pg=home" class="nav" id="">Accueil</a>
-        <a href="./?pg=photo" class="nav" id="">Galerie</a>
-        <a href="./?pg=lien" class="nav" id="">Liens</a>
-        <a href="./?pg=tuto" class="nav" id="">Tutoriels</a>
-        <a href="./?pg=contact" class="nav" id="">Contact</a>
-        <a href="./?pg=cv" class="nav" id="">CV</a>
-        <a href="./?pg=admin" class="nav" id="">Admin</a>
- </nav>
+
 
  <nav id="menu2">
    <span class="nav"><a href="./?pg=home" class="" id="">Accueil</a></span>
