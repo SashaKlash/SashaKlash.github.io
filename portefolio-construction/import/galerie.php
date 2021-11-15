244daee212db74243801dde980fb72477de268e0
SashaKlash <sashaderuyver@gmail.com>
2021-11-15 08:11:27 +0100

    copie /images pour inclure la galerie du tuto

--- portefolio-construction/import/galerie.php
+++ portefolio-construction/import/galerie.php
@@ -13,21 +13,37 @@
 <body>
     
     <?php
-    // on peut importer le menu en PHP sans passer par index.php
     include "header.php";
     ?>
     <h1>Galerie</h1>
 
-    <section id="">
-      <h2>Galerie Photo</h2>
-      <div class="gallery-img img"></div>
-      <div class="gallery-img img"></div>
-      <div class="gallery-img img"></div>
-      <div class="gallery-img img"></div>
-      <div class="gallery-img img"></div>
-      <div class="gallery-img img"></div>
-      <div class="gallery-img img "></div>
+    <section id="index-gallery" class="wrapper-gallery">
+        <p>Sasha De Ruyver</p>
+        <h2>Ma Galerie Photo</h2>
+        <div class="gallery-img img1">
+            <div><a href="/portefolio-construction/images/img1.JPG">This is a title</a></div>
+        </div>
+        <div class="gallery-img img2">
+            <div><a href="/portefolio-construction/images/img2.JPG">This is a title</a></div>
+        </div>
+        <div class="gallery-img img3">
+            <div><a href="/portefolio-construction/images/img3.JPG">This is a title</a></div>
+        </div>
+        <div class="gallery-img img4">
+            <div><a href="/portefolio-construction/images/img4.JPG">This is a title</a></div>
+        </div>
+        <div class="gallery-img img5">
+            <div><a href="/portefolio-construction/images/img5.JPG">This is a title</a></div>
+        </div>
+        <div class="gallery-img img6">
+            <div><a href="/portefolio-construction/images/img6.JPG">This is a title</a></div>
+        </div>
+        <div class="gallery-img img7">
+            <div><a href="/portefolio-construction/images/img7.JPG">This is a title</a></div>
+        </div>
     </section>
+
+    <script src="js/gallery.js"></script>
 
     <?php
       include_once "footer.php"
